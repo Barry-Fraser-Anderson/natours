086ed94c0c8d0737eac9c94114ed69ca524f6884
Barry-Fraser-Anderson <barryanderson60@outlook.com>
2022-12-08 21:16:02 +0000

    Extended tours model. Added import utility.

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+const { exit } = require('process');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connecton successful');
+  });
+
+// READ FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log(`${tours.length} tours loaded`);
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// CLEAR ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('All tours deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
