6e775fd5703796f89110463edfdc21ebe3ef7fce
Barry-Fraser-Anderson <barryanderson60@outlook.com>
2022-12-21 14:16:31 +0000

    Added 'updateMe'

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,14 @@ const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys.forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 // Get all users
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -12,6 +20,28 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+// Update logged-in user
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // Create error if users POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('Password change not allowed', 400));
+  }
+
+  // Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,6 +15,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 // prettier-ignore
 router
   .route('/')
