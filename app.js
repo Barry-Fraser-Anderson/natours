17221d6335ca6e13997f33f5f1729248f8d1750c
Barry-Fraser-Anderson <barryanderson60@outlook.com>
2022-12-21 19:59:07 +0000

    Secure HTTP headers, data sanitization, XSS, parameter pollution

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +14,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
+//
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -21,7 +31,28 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Data sanitization vs NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization vs XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupsize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
