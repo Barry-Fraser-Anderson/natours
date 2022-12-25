ca88b39c940e5f5eceab282b6dd69ccce642f011
Barry-Fraser-Anderson <barryanderson60@outlook.com>
2022-12-25 18:22:35 +0000

    Added 'distances' routepoint

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -112,6 +112,40 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  if (!lat || !lng) {
+    next(new AppError('Please provide location in the format lat,lng', 400));
+  }
+
+  const multiplier = unit === 'mi' ? 0.00623171 : 0.001;
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [+lng, +lat],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
+});
 exports.getAllTours = factory.getAll(Tour);
 exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 exports.createTour = factory.createOne(Tour);
