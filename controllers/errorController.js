d0d80073bb8afb96f5bb3950a60f96d475d980a2
Barry-Fraser-Anderson <barryanderson60@outlook.com>
2022-12-27 23:51:15 +0000

    Rendered error pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -23,32 +23,58 @@ const handleJWTExpiredError = () =>
   new AppError('Token has expired. Please log in', 401);
 
 // Development error
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+
+  // RENDERED WEBSITE
+  console.error('ERROR💥', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: err.message,
   });
 };
 
 // Production error
-const sendErrorProd = (err, res) => {
+const sendErrorProd = (err, req, res) => {
+  console.error('PROD ERROR💥', err);
   // Operation, trusted error: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  } else {
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+
     // Programming or unknown error: don't leak eror details
     console.error('ERROR💥', err);
-
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'Something went wrong!',
     });
   }
+
+  // RENDERED WEBSITE
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message,
+    });
+  }
+
+  // Programming or unknown error: don't leak eror details
+  console.error('ERROR💥', err);
+  return res.status(500).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Please try again later',
+  });
 };
 
 // Error middleware
@@ -56,11 +82,14 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+  const nodeEnv = process.env.NODE_ENV.trim();
+
+  if (nodeEnv === 'development') {
+    sendErrorDev(err, req, res);
   }
-  if (process.env.NODE_ENV === 'production') {
+  if (nodeEnv === 'production') {
     let error = { ...err };
+    error.message = err.message;
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldDB(error);
@@ -69,6 +98,7 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'JsonWebTokenError') error = handleJWTError();
     if (error.name === 'JsonWebExpiredError') error = handleJWTExpiredError();
 
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
+  //TODO: handle unrecognised nodeEnv value
 };
