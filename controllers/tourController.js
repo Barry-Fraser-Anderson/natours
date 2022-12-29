15de12a18d8bc9d99df01a5a799d2e5e72dd13de
Barry-Fraser-Anderson <barryanderson60@outlook.com>
2022-12-29 14:49:45 +0000

    Handling multiple tour images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,58 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('../controllers/handlerFactory');
 const AppError = require('../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cbk) => {
+  if (file.mimetype.startsWith('image')) {
+    cbk(null, true);
+  } else {
+    cbk(new AppError('Not an image!', 404), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  filter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // Images
+  req.body.images = [];
+  await Promise.all(
+    req.files.images.forEach(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
+});
 
 // Get 'top5' tours
 exports.aliasTopTours = async (req, res, next) => {
